2518d10883017b53a9243fa85c202afe3742ed0b
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-07-17 03:51:20 +0900

    Tweak styles and enable to sign out

--- src/components/GithubAuthButton.vue
+++ src/components/GithubAuthButton.vue
@@ -6,18 +6,21 @@ a.github-auth-button(:href="this.$store.state.auth.authUrl") GITHUB AUTH
 export default {
   name: 'github-auth-button',
   beforeMount() {
-    this.$store.dispatch('loadAuthUrl');
+    this.$store.dispatch('getAuthUrl');
   },
 };
 </script>
 
 <style lang='scss'>
+@import 'src/styles/base';
+
 .github-auth-button {
-  border: none;
+  border: 1px solid $orange1;
   border-radius: 4px;
-  width: 100px;
-  height: 24px;
-  cursor: pointer;
+  padding: 8px;
+  font-size: 16px;
+  color: $orange1;
+  text-decoration: none;
 
   &:focus {
     outline: none;
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,9 @@
 <template lang='pug'>
 section.project
-  github-auth-button
+  template(v-if="this.$store.state.auth.accessToken")
+    p Signed in
+    button(@click="signOut") Sign out
+  github-auth-button(v-else)
 </template>
 
 <script>
@@ -9,6 +12,14 @@ import GithubAuthButton from './GithubAuthButton';
 export default {
   name: 'project',
   components: { GithubAuthButton },
+  methods: {
+    signOut() {
+      this.$store.dispatch('clearAccessToken');
+    },
+  },
+  beforeMount() {
+    this.$store.dispatch('loadAccessToken');
+  },
 };
 </script>
 
