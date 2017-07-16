d7d0f587c43a6076b4f6571fc8810b0438744d81
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-07-17 00:30:37 +0900

    Create API client with axios

---
+++ src/services/Api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import { camelizeKeys, decamelizeKeys } from 'humps';
+
+// === Constants ===
+const Timeout = 15 * 1000;
+
+// FIXME: change endpoint with environment
+const ApiEndpoint = 'http://localhost:5000';
+
+const baseConfig = {
+  timeout: Timeout,
+  withCredentials: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+};
+
+// === Client Creator ===
+const requestInterceptor = (config) => {
+  if (config.data instanceof Object) {
+    config.data = decamelizeKeys(config.data);
+  }
+  if (config.params instanceof Object) {
+    config.params = decamelizeKeys(config.params);
+  }
+  return config;
+};
+
+const successResponseInterceptor = (response) => {
+  if (response.data instanceof Object) {
+    response.data = camelizeKeys(response.data);
+  }
+  return { response };
+};
+
+const errorResponseInterceptor = (error) => {
+  if (error.response && error.response.data instanceof Object) {
+    error.response.data = camelizeKeys(error.response.data);
+  }
+  return { error };
+};
+
+export function createClient(config) {
+  const mergedConfig = { ...baseConfig, ...config };
+  const client = axios.create(mergedConfig);
+  client.interceptors.request.use(requestInterceptor);
+  client.interceptors.response.use(successResponseInterceptor, errorResponseInterceptor);
+  return client;
+}
+
+// === API client ===
+export default createClient({
+  baseURL: ApiEndpoint,
+});
