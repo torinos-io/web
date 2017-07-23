93da2310f895b8660d1cb520e9bd6fe7ec2449df
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-07-23 19:47:48 +0900

    Add sign out callback component

---
+++ src/components/SignOutCallback.vue
@@ -0,0 +1,24 @@
+<template lang='pug'>
+section.auth-callback
+  h3 Redirecting ...
+</template>
+
+<script>
+import router from '@/router';
+
+export default {
+  name: 'auth-callback',
+  mounted() {
+    this.$store.dispatch('clearAccessToken');
+
+    // Delay to redirect.
+    setTimeout(() => {
+      router.push({ name: 'root' });
+    }, 1000);
+  },
+};
+</script>
+
+<style lang='scss'>
+
+</style>
